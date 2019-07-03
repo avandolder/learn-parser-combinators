854d54850c6ac99e397ea429eb4d99a6db5f7168
Adam Vandolder <adam.vandolder@gmail.com>
2019-07-03 15:26:22 -0400

    Added parent_element, whitespace_wrap

--- src/lib.rs
+++ src/lib.rs
@@ -297,12 +297,23 @@ where
 }
 
 fn element<'a>() -> impl Parser<'a, Element> {
-    either(single_element(), open_element())
+    whitespace_wrap(either(single_element(), parent_element()))
 }
 
 fn close_element<'a>(expected_name: String) -> impl Parser<'a, String> {
     right(match_literal("</"), left(identifier, match_literal(">")))
         .pred(move |name| name == &expected_name)
+}
+
+fn parent_element<'a>() -> impl Parser<'a, Element> {
+    open_element().and_then(|el| {
+        left(zero_or_more(element()), close_element(el.name.clone())).map(
+            move |children| {
+                let mut el = el.clone();
+                el.children = children;
+                el
+            })
+    })
 }
 
 fn and_then<'a, P, F, A, B, NextP>(parser: P, f: F) -> impl Parser<'a, B>
@@ -313,6 +324,13 @@ where
 {
     move |input| parser.parse(input)
                        .and_then(|(input, result)| f(result).parse(input))
+}
+
+fn whitespace_wrap<'a, P, A>(parser: P) -> impl Parser<'a, A>
+where
+    P: Parser<'a, A>,
+{
+    right(space0(), left(parser, space0()))
 }
 
 #[cfg(test)]
@@ -448,5 +466,46 @@ mod tests {
             )),
             single_element().parse("<div class=\"float\"/>")
         );
+    }
+
+    #[test]
+    fn xml_parser() {
+        let doc = r#"
+            <top label="Top">
+                <semi-bottom label="Bottom"/>
+                <middle>
+                    <bottom label="Another bottom"/>
+                </middle>
+            </top>"#;
+        let parsed_doc = Element {
+            name: "top".to_string(),
+            attr: vec![("label".to_string(), "Top".to_string())],
+            children: vec![
+                Element {
+                    name: "semi-bottom".to_string(),
+                    attr: vec![("label".to_string(), "Bottom".to_string())],
+                    children: vec![],
+                },
+                Element {
+                    name: "middle".to_string(),
+                    attr: vec![],
+                    children: vec![Element {
+                        name: "bottom".to_string(),
+                        attr: vec![("label".to_string(), "Another bottom".to_string())],
+                        children: vec![],
+                    }],
+                },
+            ],
+        };
+        assert_eq!(Ok(("", parsed_doc)), element().parse(doc));
+    }
+
+    #[test]
+    fn mismatched_closing_tag() {
+        let doc = r#"
+            <top>
+                <bottom/>
+            </middle>"#;
+        assert_eq!(Err("</middle>"), element.parse(doc));
     }
 }
