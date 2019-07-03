57ca77c36f406f6e79201d7f3593840e95016117
Adam Vandolder <adam.vandolder@gmail.com>
2019-07-02 22:12:59 -0400

    Added match_literal and corresponding test

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,40 @@
+// Following https://bodil.lol/parser-combinators/
+
+use std::collections::HashMap;
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct Element {
+    name: String,
+    attr: HashMap<String, String>,
+    children: Vec<Element>,
+}
+
+fn match_literal(expected: &'static str)
+        -> impl Fn(&str) -> Result<(&str, ()), &str> {
+    move |input| match input.get(0..expected.len()) {
+        Some(next) if next == expected => {
+            Ok((&input[expected.len()..], ()))
+        },
+        _ => Err(input),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn literal_parser() {
+        let parse_joe = super::match_literal("Hello Joe!");
+        assert_eq!(
+            Ok(("", ())),
+            parse_joe("Hello Joe!")
+        );
+        assert_eq!(
+            Ok((" Hello Robert!", ())),
+            parse_joe("Hello Joe! Hello Robert!")
+        );
+        assert_eq!(
+            Err("Hello Mike!"),
+            parse_joe("Hello Mike!")
+        );
     }
 }
